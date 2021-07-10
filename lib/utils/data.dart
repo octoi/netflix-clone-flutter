6514b837ef31a501823917d3a95a5916d1d91a1b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-10 20:41:23 +0530

    sample images

--- lib/utils/data.dart
+++ lib/utils/data.dart
@@ -1,11 +1,6 @@
-class Item {
-  String bgUrl;
-  String titleUrl;
-  List<String> tags;
-
-  Item({
-    required this.bgUrl,
-    required this.titleUrl,
-    required this.tags,
-  });
-}
+List<String> images = [
+  'https://i1.wp.com/www.comicsbeat.com/wp-content/uploads/2018/06/Stranger-Things-1b.jpg',
+  'https://th.bing.com/th/id/OIP.YbhPgO9vER_terLidoU2vgHaK-?pid=ImgDet&rs=1',
+  'https://th.bing.com/th/id/OIP.hsd1AuZOqyC5pBrp2CmNmQHaK-?pid=ImgDet&rs=1',
+  'https://th.bing.com/th/id/OIP.DZ6cdDbQStoBcl7VeJi3WgHaK9?pid=ImgDet&rs=1',
+];
