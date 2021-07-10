f17862dd6487ec53c0525fafc9634b075d140c6f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-10 18:41:15 +0530

    text

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,10 +12,36 @@ class HomeScreen extends StatelessWidget {
         elevation: 0.0,
         backgroundColor: appPrimaryColor,
         leading: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(15.0),
           child: Image(
             image: NetworkImage(netflixLogo),
           ),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              "TV Shows",
+              style: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 18.0,
+              ),
+            ),
+            Text(
+              "Movies",
+              style: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 18.0,
+              ),
+            ),
+            Text(
+              "My Lists",
+              style: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 18.0,
+              ),
+            ),
+          ],
         ),
       ),
     );
