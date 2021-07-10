d1e11109b458514deb1ec9e17db694deb9ca3161
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-10 18:33:49 +0530

    colors

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,2 +1,9 @@
-String netflixLogo =
+import 'package:flutter/material.dart';
+
+const String netflixLogo =
     'https://file.wikipediam.org/wikipedia/commons/thumb/0/0c/Netflix_2015_N_logo.svg/370px-Netflix_2015_N_logo.svg.png';
+
+const Color appPrimaryColor = Color(0xFF171717);
+const Color appSecondaryColor = Color(0xFF111111);
+const Color appPrimaryTextColor = Color(0xFFffffff);
+const Color appSecondaryTextColor = Color(0xFF585858);
