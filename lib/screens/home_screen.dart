04ee9d596a3ee024ee968d68d987d811c5bee6ac
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-10 20:55:56 +0530

    removed commented app bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,42 +9,6 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: appPrimaryColor,
-      // appBar: AppBar(
-      //   elevation: 0.0,
-      //   backgroundColor: Colors.transparent,
-      //   leading: Padding(
-      //     padding: const EdgeInsets.all(15.0),
-      //     child: Image(
-      //       image: NetworkImage(netflixLogo),
-      //     ),
-      //   ),
-      //   title: Row(
-      //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //     children: [
-      //       Text(
-      //         "TV Shows",
-      //         style: TextStyle(
-      //           fontWeight: FontWeight.normal,
-      //           fontSize: 18.0,
-      //         ),
-      //       ),
-      //       Text(
-      //         "Movies",
-      //         style: TextStyle(
-      //           fontWeight: FontWeight.normal,
-      //           fontSize: 18.0,
-      //         ),
-      //       ),
-      //       Text(
-      //         "My Lists",
-      //         style: TextStyle(
-      //           fontWeight: FontWeight.normal,
-      //           fontSize: 18.0,
-      //         ),
-      //       ),
-      //     ],
-      //   ),
-      // ),
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
         child: Column(
