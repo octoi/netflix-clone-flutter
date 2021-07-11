ff7e7736dd7bd534724f3782acbdb7dda8272198
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-11 12:53:22 +0530

    added sized box

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,6 +14,7 @@ class HomeScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             HeaderSection(),
+            SizedBox(height: 20.0),
             ListSection(),
           ],
         ),
