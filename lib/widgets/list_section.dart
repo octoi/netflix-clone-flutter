c66bd637d37daf3f9330e85b4b11c2a44fed552a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-11 12:33:57 +0530

    used item list

--- lib/widgets/list_section.dart
+++ lib/widgets/list_section.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/widgets/item_list.dart';
 
 class ListSection extends StatelessWidget {
   const ListSection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        ItemList(title: "Popular On Netflix"),
+        ItemList(title: "Trending Now"),
+        ItemList(title: "Watch It Again"),
+      ],
+    );
   }
 }
