383826c498ce12671b52adfe2db2ea1086701463
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-11 12:13:03 +0530

    list section

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,6 +14,7 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             HeaderSection(),
+            ListSection(),
           ],
         ),
       ),
