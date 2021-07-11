936501dcce9f93908316aceca748ad6b3ef8084d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-11 12:34:01 +0530

    displayed title

---
+++ lib/widgets/item_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ItemList extends StatelessWidget {
+  final String title;
+
+  const ItemList({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 10.0),
+        Text(title),
+      ],
+      // SingleChildScrollView(
+      //     physics: BouncingScrollPhysics(),
+      //     scrollDirection: Axis.horizontal,
+      //     child: Row(children: getItems()),
+      //   )
+    );
+  }
+}
